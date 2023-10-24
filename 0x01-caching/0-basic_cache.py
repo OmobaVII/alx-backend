1b59a6a34793a16fb6b17495e1dd7a117646b728
OmobaVII <omoba.career@gmail.com>
2023-10-24 18:22:48 +0000

    implement classes

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -11,20 +11,20 @@ class BasicCache(BaseCaching):
     inherits from BaseCaching"""
     def __init__(self):
         """ Initiliaze"""
-        self.cache_data = {}
+        super().__init__()
 
     def put(self, key, item):
         """assign to the dictionary self.cache_data"""
-        if key != None or item != None:
+        if key is not None or item is not None:
             self.cache_data[key] = item
         else:
             pass
 
     def get(self, key):
         """returns the value in self.cache_data liked to key"""
-        if key == None or key not in self.cache_data.keys():
-            return None
-        else:
+        if key is not None or key in self.cache_data.keys():
             for k, v in self.cache_data.items():
                 if k == key:
                     return v
+        else:
+            return None
