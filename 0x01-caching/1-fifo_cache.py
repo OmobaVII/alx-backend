9facd23de2293553d6c034ad79f12dc1ae4493c2
OmobaVII <omoba.career@gmail.com>
2023-10-25 11:10:26 +0000

    Implement FIFO caching

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -27,7 +27,7 @@ class FIFOCache(BaseCaching):
 
     def get(self, key):
         """returns the value of the key"""
-        if key is None:
+        if key is None or key not in self.cache_data:
             return None
-        if key in self_cache_data:
+        else:
             return self.cache_data[key]
