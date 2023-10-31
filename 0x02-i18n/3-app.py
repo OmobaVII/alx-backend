476a8043e6669821fc790b581b8a5da2a1d8a0fc
OmobaVII <omoba.career@gmail.com>
2023-10-31 17:56:40 +0000

    Implement translation

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+""" This module instantiate Babel object in the app """
+from flask import Flask, render_template, request
+from flask_babel import Babel
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    """ a class that configures the app """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """ returns 0-index.html page """
+    return render_template('3-index.html')
+
+
+@babel.localeselector
+def get_locale():
+    """ determines the best match for the user's
+    preferred languages """
+    return request.accept_languages.best_match(app.config.languages)
+
+
+if __name__ == "__main__":
+    app.run()
