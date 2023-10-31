aa39513fbfc47575ae24aef7da30292c9ada82ed
OmobaVII <omoba.career@gmail.com>
2023-10-31 16:12:46 +0000

    Basic Babel setup

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+""" This module instantiate Babel object in the app """
+from flask import Flask, render_template
+from flask_babel import Babel
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """ returns 0-index.html page """
+    return render_template('1-index.html')
+
+
+if __name__ == "__main__":
+    app.run()
