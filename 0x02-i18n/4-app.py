1df2984af7cb5b1312b660edc5f176d44305876d
OmobaVII <omoba.career@gmail.com>
2023-10-31 22:22:47 +0000

    Implement

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """ This module instantiate Babel object in the app """
 from flask import Flask, render_template, request
-from flask_babel import Babel
+from flask_babelex import Babel
 app = Flask(__name__)
 babel = Babel(app)
 
@@ -13,25 +13,25 @@ class Config:
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
-app.config.from_object(Config)
+app.config.from_object('4-app.Config')
 
 
 @app.route('/', strict_slashes=False)
-def index():
+def index() -> str:
     """ returns 0-index.html page """
     return render_template('4-index.html')
 
 
-# @babel.localeselector
-def get_locale():
+@babel.localeselector
+def get_locale() -> str:
     """ determines the best match for the user's languages """
     if request.args.get('locale'):
         locale = request.args.get('locale')
         if locale in app.config['LANGUAGES']:
             return locale
     else:
-        return request.accept_languages.best_match(app.config.languages)
+        return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0', port=5000)
